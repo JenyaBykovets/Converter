81ee343cd30af8bcbba01a7ccabc9962a9b805cb
Jeka <jenyabikovec@gmail.com>
2024-01-23 00:19:29 +0200

    Update app info

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -3,6 +3,7 @@ package app;
 import java.util.Scanner;
 
 public class Converter {
+    // miles - kilometers
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter miles: ");
